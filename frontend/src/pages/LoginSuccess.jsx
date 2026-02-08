4075409afdb13d57ff60d1bb3e339273cfb84973
Sartaj Alam <Sartaj9806@gmail.com>
2026-02-08 22:27:59 +0530

    Update login page

--- frontend/src/pages/LoginSuccess.jsx
+++ frontend/src/pages/LoginSuccess.jsx
@@ -10,11 +10,17 @@ function LoginSuccess() {
 
     if (token) {
       localStorage.setItem("token", token);
-      navigate("/dashboard");
+      navigate("/dashboard"); // redirect to dashboard
+    } else {
+      navigate("/");
     }
   }, []);
 
-  return <p>Logging in...</p>;
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h2 className="text-xl font-semibold">Logging you in...</h2>
+    </div>
+  );
 }
 
 export default LoginSuccess;
